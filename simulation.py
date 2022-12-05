ea2717367b316692f19b3d8c148f5092b658a2ec
Robin <robin.stassen2@gmail.com>
2022-12-05 16:56:43 +0100

    Cleaned up the main-loop and satellitePRISM function. Still need to check/refactor the satellitePrism function.

--- simulation.py
+++ simulation.py
@@ -2,13 +2,13 @@ import math as m
 import matplotlib.pyplot as plt
 import numpy as np
 # import astropy.constants as c
-
 def orbit_cartesian(orbit_inclination, orbit_right_ascension, time): #time is in radians
     x = m.sin(time+orbit_right_ascension)*m.cos(orbit_inclination)
     y = m.sin(time-orbit_right_ascension)*m.sin(orbit_inclination)
     z = m.cos(time+orbit_right_ascension)
     coordinate = [x,y,z]
     return coordinate
+
 def orbit_spherical(inclination, right_ascension, time): #time is in seconds
     clockangle = (time/period)*2*m.pi
     latitude = m.sin(clockangle)*inclination
@@ -21,18 +21,21 @@ def orbit_spherical(inclination, right_ascension, time): #time is in seconds
         longitude += m.pi
     coordinate = [latitude, longitude]
     return coordinate
+
 def STXY(latitude, longitude): #lo = longitude is perpendicular to equator, la = latitude is parallel to equator
     #Gall-Peters projection
     x = longitude%(2*m.pi) #longitude in radians from central meridian
     y = 2*m.sin(latitude) #latitude in radians from equator
     coordinate = [x,y]
     return coordinate
+
 def SunPosition(time):
     Earth_tilt = (23.5/180)*m.pi #Earth's tilt in radians
     solar_inclination = Earth_tilt*m.sin((time/(day*365.25))*2*m.pi) #declination is 0 on March 21st (80th day of the year)
     solar_right_ascension = (time/day)*2*m.pi #right ascension of sun rising
     position = [solar_inclination, solar_right_ascension]
     return position
+
 def sunCircle(time):
     Earth_tilt = (23.5/180)*m.pi #Earth's tilt in radians
     solar_inclination = Earth_tilt*m.sin((time/(day*365.25))*2*m.pi) #declination is 0 on March 21st (80th day of the year)
@@ -50,6 +53,7 @@ def sunCircle(time):
             pos[i,0] = -(m.pi + pos[i,0])
             pos[i,1] += m.pi
     return pos
+
 def circle(inclination, right_ascension):
     r = 0.5*m.pi
     n = 4
@@ -58,11 +62,13 @@ def circle(inclination, right_ascension):
         theta = i*((2*m.pi)/n)
         pos[i] = [m.sin(theta)*r + inclination, m.cos(theta)*r + right_ascension]
     return pos
+
 def period(a):
     mu = 3.986E14
     period = 2*m.pi*m.sqrt((a**3)/mu)
     return period
-def satelliteEllipse(inclination, right_ascension, time):
+
+def satellitePrism(inclination, right_ascension, time):
     #satellite position
     clockangle = (time/period)*2*m.pi
     latitude = m.sin(clockangle)*inclination
@@ -75,65 +81,37 @@ def satelliteEllipse(inclination, right_ascension, time):
         longitude += m.pi
     coordinate = [latitude, longitude]
     
-    #visible area ellipse
-    #width = (10/180)*m.pi #longitude
-    #height = (45/180)*m.pi #latitude
+    #visible area prism
+    width = (6.08/180)*m.pi  #function of altitude and observing time
+    height = (30/180)*m.pi     #function of altitude and observing time
     
-    height = (4.2/180)*m.pi  #function of altitude and observing time 
-    width = (2/180)*m.pi     #function of altitude and observing time
-    
+    #rotation
     direction = m.cos(clockangle)*inclination   #angle of the direction of the orbit, 0 = horizontal
-    x1 = height
-    x2 = (2*m.pi*(measure_time/period) - height)
-    x3 = x2
-    x4 = x2
-    y1 = width
-    y2 = -width
-    y3 = y1
-    y4 = y2
-    x1rotated = x1*m.cos(direction) - y1*m.sin(direction)      #rotation matrix for 2d, CCW rotation
-    y1rotated = x1*m.sin(direction) + y1*m.cos(direction)
-    x2rotated = x2*m.cos(direction) - y2*m.sin(direction)      #rotation matrix for 2d, CCW rotation
-    y2rotated = x2*m.sin(direction) + y2*m.cos(direction)
-    x3rotated = x3*m.cos(direction) - y3*m.sin(direction)
-    y3rotated = x3*m.sin(direction) + y3*m.cos(direction)
-    x4rotated = x4*m.cos(direction) - y4*m.sin(direction)
-    y4rotated = x4*m.sin(direction) + y4*m.cos(direction)
-    coord1 = [x1rotated + coordinate[0], y1rotated + coordinate[1]]
-    coord2 = [x2rotated + coordinate[0], y2rotated + coordinate[1]]
-    coord3 = [x3rotated + coordinate[0], y3rotated + coordinate[1]]
-    coord4 = [x4rotated + coordinate[0], y4rotated + coordinate[1]]
-    if(coord1[0] > 0.5*m.pi):
-        coord1[0] = m.pi - coord1[0]
-        coord1[1] += m.pi
-    if(coord1[0] < -0.5*m.pi):
-        coord1[0] = -(m.pi + coord1[0])
-        coord1[0] += m.pi
-    if(coord2[0] > 0.5*m.pi):
-        coord2[0] = m.pi - coord2[0]
-        coord2[1] += m.pi
-    if(coord2[0] < -0.5*m.pi):
-        coord2[0] = -(m.pi + coord2[0])
-        coord2[0] += m.pi
-    if(coord3[0] > 0.5*m.pi):
-        coord3[0] = m.pi - coord3[0]
-        coord3[1] += m.pi
-    if(coord3[0] < -0.5*m.pi):
-        coord3[0] = -(m.pi + coord3[0])
-        coord3[0] += m.pi
-    if(coord4[0] > 0.5*m.pi):
-        coord4[0] = m.pi - coord4[0]
-        coord4[1] += m.pi
-    if(coord4[0] < -0.5*m.pi):
-        coord4[0] = -(m.pi + coord4[0])
-        coord4[0] += m.pi
-    # if(pos[i,0] > 0.5*m.pi):
-    #     pos[i,0] = m.pi - pos[i,0]
-    #     pos[i,1] += m.pi
-    # if(pos[i,0] < -0.5*m.pi):
-    #     pos[i,0] = -(m.pi + pos[i,0])
-    #     pos[i,1] += m.pi
-    return [coord1,coord2,coord3,coord4]
+    point1 = [height,width]
+    point2 = [(2*m.pi*(measure_time/period) - height),-width]
+    point3 = [(2*m.pi*(measure_time/period) - height),width]
+    point4 = [height,-width]
+    rotation_matrix = [[m.cos(direction), m.sin(direction)],[-m.sin(direction), m.cos(direction)]] #CW rotation matrix
+    point1_rotated = np.matmul(rotation_matrix,point1)
+    point2_rotated = np.matmul(rotation_matrix,point2)
+    point3_rotated = np.matmul(rotation_matrix,point3)
+    point4_rotated = np.matmul(rotation_matrix,point4)
+    coord1 = [point1_rotated[0] + coordinate[0], point1_rotated[1] + coordinate[1]]
+    coord2 = [point2_rotated[0] + coordinate[0], point2_rotated[1] + coordinate[1]]
+    coord3 = [point3_rotated[0] + coordinate[0], point3_rotated[1] + coordinate[1]]
+    coord4 = [point4_rotated[0] + coordinate[0], point4_rotated[1] + coordinate[1]]
+    coords = [coord1,coord2,coord3,coord4]
+    
+    #overflowing
+    for i in range(len(coords)):
+        if(coords[i][0] > 0.5*m.pi):
+            coords[i][0] = m.pi - coords[i][0]
+            coords[i][1] += m.pi
+        elif(coords[i][0] < -0.5*m.pi):
+            coords[i][0] = -(m.pi + coords[i][0])
+            coords[i][1] += m.pi
+    return coords
+
 def ellipse(inclination, right_ascension):
     n = 4
     pos = np.empty([n,2])
@@ -144,44 +122,26 @@ def ellipse(inclination, right_ascension):
         pos[i] = [m.sin(theta)*height + inclination,m.cos(theta)*width + right_ascension]
     return pos
 
-
 #parameters
 timestamp_length = 180 #seconds
 day = 86164 #seconds, 365.25/366.25 * 24 * 3600, seconds in a sidereal day
 
 #inputs for multiple satellites
-inclinations = [(50/180)*m.pi]#, (80/180)*m.pi]
-right_ascensions = [(20/180)*m.pi]#, (50/180)*m.pi]
-altitudes = 200*10**3 #meters
+inclinations = [(20/180)*m.pi] #radians
+right_ascensions = [(20/180)*m.pi] #radians
+altitudes = 700*10**3 #meters
+timelength = 10*timestamp_length #seconds
+
 a = 6371000 + altitudes #meters
 period = int(period(a)) #seconds
-timelength = timestamp_length
-n = int(timelength/timestamp_length)
+n = int(timelength/timestamp_length) 
 orbits = timelength/period
 days = timelength/day
 r_earth = 6371*1000 #meters
 measure_time = 120 #seconds
 
-#orbit plot 2d
-# n = timelength/timestamp_length
-# inclination = (45/180)*m.pi
-# right_ascension = (10/180)*m.pi
-# positions = np.empty([int(n),3])
-# for i in range(int(n)):
-#     time = (i*(timestamp_length/period)*2*m.pi)
-#     point = orbit_cartesian(inclination, right_ascension, time)
-#     positions[i] = point
-# plt.figure()
-# plt.xlim(-1,1)
-# plt.ylim(-1,1)
-# plt.plot(positions[:,0], positions[:,1])
-# plt.show()
-
-#2d projection
+#figure
 image = plt.imread("Gall–Peters_projection.jpg")
-positions = np.empty([n,2])
-positions_sun = np.empty([n,2])
-
 fig = plt.figure()
 fig, ax = plt.subplots()
 # for i in range(int(days)):
@@ -190,21 +150,23 @@ fig, ax = plt.subplots()
 #     string = ax.imshow(image, extent = [i*2*m.pi,(1+i)*2*m.pi,-2,2])
 img = image
 img = ax.imshow(image, extent = [0,2*m.pi,-2,2])
-
 plt.xlim(0,2*m.pi)
 plt.ylim(-2,2) 
 
-#main loop
+#Main loop
+positions = np.empty([n,2])
+positions_sun = np.empty([n,2])
 for i in range(n):
-    time = i*timestamp_length + 2505.8*day
+    time = i*timestamp_length + 2505.82*day
     for j in range(len(inclinations)):
+        #Satellite
         point_spherical = orbit_spherical(inclinations[j],right_ascensions[j],time)
         point_projection = STXY(point_spherical[0],point_spherical[1])
         positions[j] = point_projection
-        print(positions)
-        #plt.plot(positions[:,0], positions[:,1], color = 'red')
+        plt.scatter(positions[j,0], positions[j,1], color = 'red', s = 1)
         
-        visibleRegion = satelliteEllipse(inclinations[j], right_ascensions[j], time)
+        #Visible area
+        visibleRegion = satellitePrism(inclinations[j], right_ascensions[j], time)
         pos1 = visibleRegion[0]
         pos2 = visibleRegion[1]
         pos3 = visibleRegion[2]
@@ -213,37 +175,21 @@ for i in range(n):
         pos2_proj = STXY(pos2[0],pos2[1])
         pos3_proj = STXY(pos3[0],pos3[1])
         pos4_proj = STXY(pos4[0],pos4[1])
-        plt.scatter(pos1_proj[0],pos1_proj[1], color='red', s=0.1)
-        plt.scatter(pos2_proj[0],pos2_proj[1], color='red', s=0.1)
-        plt.scatter(pos3_proj[0],pos3_proj[1], color='red', s=0.1)
-        plt.scatter(pos4_proj[0],pos4_proj[1], color='red', s=0.1)
-        # for k in range(visibleRegion.shape[0]):
-        #     point = STXY(visibleRegion[k,0], visibleRegion[k,1])
-        #     plt.scatter(point[0], point[1], color='red', s = 10)
-        
+        plt.scatter(pos1_proj[0],pos1_proj[1], color='red', s=1)
+        plt.scatter(pos2_proj[0],pos2_proj[1], color='red', s=1)
+        plt.scatter(pos3_proj[0],pos3_proj[1], color='red', s=1)
+        plt.scatter(pos4_proj[0],pos4_proj[1], color='red', s=1)
+    #Sun    
     point_sun = SunPosition(time)
     point_sun_projection = STXY(point_sun[0], point_sun[1])
     positions_sun[i] = point_sun_projection
-    #illuminated area
+    plt.scatter(positions_sun[i,0],positions_sun[i,1], color='yellow', s=5)
+    
+    #Illuminated area
     illuminatedRegion = sunCircle(time)
     for i in range(illuminatedRegion.shape[0]):
         point = STXY(illuminatedRegion[i,0], illuminatedRegion[i,1])
-        plt.scatter(point[0], point[1], color='yellow', s = 5)
-    
-for i in range(illuminatedRegion.shape[0]):
-        point = STXY(illuminatedRegion[i,0], illuminatedRegion[i,1])
-        plt.scatter(point[0], point[1], color='yellow', s = 5)
-    
-
-#plt.scatter(positions[:,0], positions[:,1], color = 'red', s = 1)
-#plt.plot(positions[:,0], positions[:,1], color = 'red')
-plt.scatter(positions_sun[n-1,0], positions_sun[n-1,1], color='yellow', s=5)
-
-# position_custom = [(51.838035/180)*m.pi, (184.391580/180)*m.pi]
-# position_custom = STXY(position_custom[0], position_custom[1])
-# plt.scatter(position_custom[0],position_custom[1], color = 'red', s = 0.5)
-
-
+        plt.scatter(point[0], point[1], color='yellow', s=5)
 
 #AOI matrix
 # vertical = m.pi
@@ -271,6 +217,6 @@ plt.scatter(positions_sun[n-1,0], positions_sun[n-1,1], color='yellow', s=5)
 # for i in range(counter):
 #     point = STXY(AOI_interest[i,0], AOI_interest[i,1])
 #     plt.scatter(point[0], point[1], color = 'blue', s = 1)
-print("Done")
-#fig.savefig("AOI_1deg", dpi=1000)
+fig.savefig("test_square_observable.jpg", dpi=1000)
 plt.show()
+print("Done")
